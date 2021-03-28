4c6bbc50da3527d32f7b477f32ad8d4df98e3f37
Saksham Gurung <be2016se504@gces.edu.np>
2021-03-28 20:16:31 +0545

    base url is changed to use remote server.

--- src/api/loggedInClient.js
+++ src/api/loggedInClient.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 import {apiConfig} from './apiConfig'
 
 export const loggedInClient = axios.create({
-    baseURL:apiConfig.devBaseUrl
+    baseURL:apiConfig.baseUrl
 });
 
 export const getLoggedinClient = async() => {
